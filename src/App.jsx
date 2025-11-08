aaa85bae86893105dcd75de556548c3535136644
maguma <maguma5@hotmail.com>
2025-11-08 16:16:29 +0100

    regreso3

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <button onClick={() => setModo("dia")}>Ver trabajadores de hoy</button>
       <button onClick={() => setModo("mes")}>Ver trabajadores del mes</button>
 
-      {loading && <p>Cargando...</p>}
+      {/* {loading && <p>Cargando...</p>} */}
 
       <button onClick={() => setMostrar(!mostrar)}>Ver Trabajadores</button>
 
