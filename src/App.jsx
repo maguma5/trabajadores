1aced21fb0e099b5ad1f270a0bed18b530a5b432
maguma <maguma5@hotmail.com>
2025-11-10 19:34:33 +0100

    volveraregreso3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Logo from "./assets/iconosegura.jpg";
 import "./App.css";
-//import Appseleccion from "./Appseleccion";
+import { useTrabajadores } from "./libs/hooks/useTrabajadores";
 import Appseleccion from "./AppseleccionPrueba";
 
 function App() {
-  const [modoSeleccionado, setModoSeleccionado] = useState(null);
+  //const [modoSeleccionado, setModoSeleccionado] = useState(null);
+  const { trabajadores } = useTrabajadores();
+  const [mostrar, setMostrar] = useState(false);
+
   const volverInicio = () => {
     setModoSeleccionado(null);
   };
@@ -20,11 +23,16 @@ function App() {
 
           <h1>Control de Presencia</h1>
           <p>¿Qué tipo de listado quieres ver?</p>
-          <button onClick={() => setModoSeleccionado("dia")}>
-            Listar por día
+
+          <button onClick={() => setModo("dia")}>
+            Ver trabajadores de hoy
           </button>
-          <button onClick={() => setModoSeleccionado("mes")}>
-            Listar por mes
+          <button onClick={() => setModo("mes")}>
+            Ver trabajadores del mes
+          </button>
+
+          <button onClick={() => setMostrar(!mostrar)}>
+            Ver Trabajadores{" "}
           </button>
         </div>
       ) : (
