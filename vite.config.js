1aa906fe5c13adc83d194620b2395573e4d2a667
maguma <maguma5@hotmail.com>
2025-11-10 21:48:44 +0100

    newAppseleccion10

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    // Solo para desarrollo
+  },
+  // 👇 Esto es clave para Vercel
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+
   plugins: [
     react({
       babel: {
