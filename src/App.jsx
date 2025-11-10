60f06de92e31d71e127591040debe0a25a9f8ef2
maguma <maguma5@hotmail.com>
2025-11-10 19:38:42 +0100

    volveraregreso3_1

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,27 @@ function App() {
   };
 
   return (
-    <div>
-      {!modoSeleccionado ? (
+    <>
+      <h1>EMPRESA</h1>
+
+      <img src={Logo} alt="Logo de la empresa" width="200" />
+
+      <h1>Control de Presencia</h1>
+      <p>¿Qué tipo de listado quieres ver?</p>
+
+      <button onClick={() => setModo("dia")}>Ver trabajadores de hoy</button>
+      <button onClick={() => setModo("mes")}>Ver trabajadores del mes</button>
+
+      <button onClick={() => setMostrar(!mostrar)}>Ver Trabajadores </button>
+
+      {mostrar && (
         <div>
-          <h1>EMPRESA</h1>
-
-          <img src={Logo} alt="Logo de la empresa" width="200" />
-
-          <h1>Control de Presencia</h1>
-          <p>¿Qué tipo de listado quieres ver?</p>
-
-          <button onClick={() => setModo("dia")}>
-            Ver trabajadores de hoy
-          </button>
-          <button onClick={() => setModo("mes")}>
-            Ver trabajadores del mes
-          </button>
-
-          <button onClick={() => setMostrar(!mostrar)}>
-            Ver Trabajadores{" "}
-          </button>
+          {trabajadores.map((trabajador, index) => (
+            <p key={index}>{trabajador.nombre}</p>
+          ))}
         </div>
-      ) : (
-        <Appseleccion modo={modoSeleccionado} volver={volverInicio} />
       )}
-    </div>
+    </>
   );
 }
 
