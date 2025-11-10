9de6efd6de1fe7e28a9752d9c7a0786c5a8aaeb7
maguma <maguma5@hotmail.com>
2025-11-10 20:23:50 +0100

    newAppseleccion2

--- src/App.jsx
+++ src/App.jsx
@@ -14,27 +14,26 @@ function App() {
   };
 
   return (
-    <>
-      <h1>EMPRESA</h1>
-
-      <img src={Logo} alt="Logo de la empresa" width="200" />
-
-      <h1>Control de Presencia</h1>
-      <p>¿Qué tipo de listado quieres ver otra vez?</p>
-
-      <button onClick={() => setModo("dia")}>Ver trabajadores de hoy</button>
-      <button onClick={() => setModo("mes")}>Ver trabajadores del mes</button>
-
-      <button onClick={() => setMostrar(!mostrar)}>Ver Trabajadores </button>
-
-      {mostrar && (
+    <div>
+      {!modoSeleccionado ? (
         <div>
-          {trabajadores.map((trabajador, index) => (
-            <p key={index}>{trabajador.nombre}</p>
-          ))}
+          <h1>EMPRESA</h1>
+
+          <img src={Logo} alt="Logo de la empresa" width="200" />
+
+          <h1>Control de Presencia</h1>
+          <p>¿Qué tipo de listado quieres ver?</p>
+          <button onClick={() => setModoSeleccionado("dia")}>
+            Listar por día
+          </button>
+          <button onClick={() => setModoSeleccionado("mes")}>
+            Listar por mes
+          </button>
         </div>
+      ) : (
+        <Appseleccion modo={modoSeleccionado} volver={volverInicio} />
       )}
-    </>
+    </div>
   );
 }
 
