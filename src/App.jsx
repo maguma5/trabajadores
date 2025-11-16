9a89d4eb2ae1eac7a02fafc0f103622b35b73353
maguma <maguma5@hotmail.com>
2025-11-16 09:27:38 +0100

    volveratras4

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,37 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import Logo from "./assets/iconosegura.jpg";
+
+import "./App.css";
 import { useTrabajadores } from "./libs/hooks/useTrabajadores";
-//import { useEmpresas } from "./libs/hooks/useEmpresas";
 
-function Appseleccion() {
-  //const { empresas, loading1 } = useEmpresas();
-  //const { empresas } = useEmpresas();
-  const [empresaSeleccionada, setEmpresaSeleccionada] = useState("");
-  const [fechaSeleccionada, setFechaSeleccionada] = useState("");
-
-  const { trabajadores, loading } = useTrabajadores(
-    empresaSeleccionada,
-    fechaSeleccionada
-  );
+function App() {
+  const { trabajadores } = useTrabajadores();
+  const [mostrar, setMostrar] = useState(false);
 
   return (
-    <div>
-      <h1>Filtrar trabajadores</h1>
-      <label>Empresa:</label>
-      <select
-        value={empresaSeleccionada}
-        onChange={(e) => setEmpresaSeleccionada(e.target.value)}
-      >
-        <option value="">Selecciona una empresa</option>
+    <>
+      <h1>EMPRESA</h1>
 
-        {empresas.length > 0 &&
-          empresas.map((e, i) => (
-            <option key={i} value={e}>
-              {e}
-            </option>
-          ))}
-      </select>
-      <label>Fecha:</label>
-      <input
-        type="date"
-        value={fechaSeleccionada}
-        onChange={(e) => setFechaSeleccionada(e.target.value)}
-      />
+      <img src={Logo} alt="Logo de la empresa" width="200" />
 
-      {loading && <p>Cargando...</p>}
-      {!loading && trabajadores.length > 0 && (
-        <ul>
-          {trabajadores.map((t, i) => (
-            <li key={i}>{t.empresa}</li>
+      <h1>Control de Presencia</h1>
+
+      <button onClick={() => setModo("dia")}>Ver trabajadores de hoy</button>
+      <button onClick={() => setModo("mes")}>Ver trabajadores del mes</button>
+
+      {/* {loading && <p>Cargando...</p>} */}
+
+      <button onClick={() => setMostrar(!mostrar)}>Ver Trabajadores</button>
+
+      {mostrar && (
+        <div>
+          {trabajadores.map((trabajador, index) => (
+            <p key={index}>{trabajador.nombre}</p>
           ))}
-        </ul>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
-export default Appseleccion;
-
-//export default App;
+export default App;
